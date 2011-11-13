f882a95262fda5c4794a98d6a722a18b9b18d6fe
Vasco Fernandes <vasco.box@gmail.com>
2011-11-13 22:46:22 +0000

    final

--- unshredder.py
+++ unshredder.py
@@ -1,67 +1,32 @@
+#!/usr/bin/python 
+
+# Copyright (c) 2011 Vasco Fernandes
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 from PIL import Image
 import sys
-import numpy
-from pprint import pprint
-
-#def compare_columns(img, c1, c2):
-#    w,h = img.size
-#    data = img.getdata()
-#
-#    diff = []
-#    for i in range(0, w*h, w):
-#        left = data[i+c1]
-#        right = data[i+c2]
-#        #Color does not hold any information
-#        pixel_diff = sum(map(lambda x,y: abs(x-y), left, right))/len(left)
-#        print pixel_diff
-#        diff.append(pixel_diff)
-#    return sum(diff)
+import argparse
 
 
-#def make_difference_image(img):
-#    w,h = img.size
-#    data = img.getdata()
-#    diff = []
-
-#    diff_image = Image.new('RGB', img.size)
-
-#    for i in range(w*h-1):
-#        x = i % w
-#        y = i / w
-#        left = data[i]
-#        right = data[i+1]
-
-#        pixel = [(sum(map(lambda x,y: 255-abs(x-y), left, right))-255)/3]*3
-#        diff_image.putpixel((x, y), tuple(pixel[:3]))
-
-    
-#    print diff
-#    diff_image.save('test.png')
-
-#def most_likely_values(dividers, bw, w):
-#    columns = [bw[i::w] for i in range(0, w)]
-
-#    new_dividers = []
-#    for right in  dividers:
-#        best = right-1, 1000000
-#        for left in dividers:
-#            left -= 1
-#            if left == right: continue
-#            entropy = sum(map(lambda x, y: abs(x-y), columns[left], columns[right]))
-#            if entropy < best[1]:
-#                best = (left, entropy) 
-#            #print "%d -> %d: %d" % (left, right, entropy)
-#        #return
-#        if not right == best[0]+1:
-#            new_dividers.append(right)
-#            print "%d <- %d: %d" % (right, best[0], best[1])
-#    
-#    if not len(new_dividers) == len(dividers):
-#        return most_likely_values(new_dividers, bw, w)
-
-    
-    #for b in blocks:
-    #    print "[%d -- %d]" % b
 
 class ShreddedImage(object):
     def __init__(self, img):
@@ -73,10 +38,20 @@ class ShreddedImage(object):
         self.bw = map(sum, self.data)
         self.columns = [self.bw[i::self.w] for i in range(0, self.w)]
 
-    def diff(self, x, y):
-        return sum(map(lambda x, y: abs(x-y), self.columns[x], self.columns[y]))
+    def diff(self, x, y, cache={}):
+        if (x, y) in cache:
+            return cache[(x, y)]
+        else:
+            r = sum(map(lambda x, y: abs(x-y), self.columns[x], self.columns[y]))
+            cache[(x, y)] = r
+            return r
 
     def entropy(self, c1, c2, cache={}):
+        """Calculate the entropy of a transition between column c1 and c2.
+        A a small value indicates the the rhythm of change is maintained 
+        throughout the boundary."""
+
+        if c1 == c2: return sys.maxint
         if (c1, c2) in cache:
             return cache[(c1, c2)]
         else:
@@ -94,188 +69,151 @@ class ShreddedImage(object):
                 right += self.diff(c2+1, c2+2)
                 right /= 2
 
-
-            r = (e-left) + (e-right)
+            r = abs((e-left)) + abs((e-right))
 
             cache[(c1, c2)] = r
             return r
 
-    def gess_blocks(self):
+    def set_dividers(self, dividers):
+        dividers = [d for d in sorted(dividers) if d > 0 and d < self.w]
+        self.blocks = map(lambda x, y: {'start': x, 'end': y-1, 'parts':[(x, y-1)]}, [0] + dividers, dividers + [self.w])
+
+    def guess_dividers(self, even=False):
+        #create the difference image from the greyscale data
         diff = map(lambda x, y: abs(x-y), self.bw, self.bw[1:]+[0])
+
+        #the average pixel difference in the image
         avg = sum(diff)/len(diff)
 
+        #divide the difference image in columns
         columns = map(lambda i: diff[i::self.w], range(self.w))
+        
+        #classify each columns as the number of pixel that changes faster than average
         columns = map(lambda a: reduce(lambda x, y: x+1 if y > avg else x, a, 0), columns)
 
-        diff_columns = map(lambda x, y: abs(x-y), [0] + columns[:-1], columns)
+        #calculate the speed of change from column to column
+        diff_columns = map(lambda x, y: y-x, [0] + columns[:-1], columns)
 
-        dividers = []
-        for i in range(1, len(diff_columns)):
-            p,v = diff_columns[i-1:i+1]
-            if v > p and v > 30:
-                dividers.append(i+1)
+        #a minimal value of change for a column to be considered a divider
+        divider_threshold = self.h / 10
 
-        blocks = map(lambda x, y: {'start': x, 'end': y-1, 'parts':[(x, y-1)]}, [0]+dividers[:-1], dividers)
-        self.blocks = blocks
+        #select the dividers 
+        dividers = [i+2 for i, (p, v) in enumerate(zip(diff_columns[:-1], diff_columns[1:])) 
+                        if v > divider_threshold]
+        
+        if even:
+            splits = map(lambda x, y: y - x, dividers[:-1], dividers[1:])
+            m = max(set(splits), key=splits.count)
+            dividers = range(0, self.w, m)
+
+        self.set_dividers(dividers)
 
     def merge(self):
+        """Merge the image blocks using a greedy algorithm according 
+        to the entropy of the blocks boundaries."""
+
         columns = self.columns
         blocks = list(self.blocks)
+        nblocks = len(blocks)
         
+        entropy_matrix = [self.entropy(b1['end'], b2['start']) for b1 in blocks 
+                                                               for b2 in blocks]
         while len(blocks) > 1:
-            entropy_matrix = {}
+           entropy_matrix = {}
 
-            for i, b1 in enumerate(blocks):
-                for j, b2 in enumerate(blocks):
-                    if not i == j:
-                        entropy_matrix[(i,j)] = self.entropy(b1['end'], b2['start'])
+           for i, b1 in enumerate(blocks):
+               for j, b2 in enumerate(blocks):
+                   if not i == j:
+                       entropy_matrix[(i,j)] = self.entropy(b1['end'], b2['start'])
 
-            ((i, j), e) = min(entropy_matrix.items(), key=lambda x: x[1])
-            b1 = blocks[i]
-            b2 = blocks[j]
+           ((i, j), e) = min(entropy_matrix.items(), key=lambda x: x[1])
 
-            new_block = {'start':b1['start'], 
-                         'end':b2['end'], 
-                         'parts': b1['parts']+b2['parts']} 
+           print e
+           b1 = blocks[i]
+           b2 = blocks[j]
 
-            if i < j:
-                del blocks[j]
-                del blocks[i]
-            else:
-                del blocks[i]
-                del blocks[j]
+           new_block = {'start':b1['start'], 
+                        'end':b2['end'], 
+                        'parts': b1['parts']+b2['parts']} 
 
-            blocks.append(new_block)
+           if i < j:
+               del blocks[j]
+               del blocks[i]
+           else:
+               del blocks[i]
+               del blocks[j]
+
+           blocks.append(new_block)
         return blocks[0]
 
+    def unshred(self, even=False, dividers=None):
+        #if no predefined blocks are given, guess them
+        if not dividers: self.guess_dividers(even)
+        else: self.set_dividers(dividers)
 
+        #duplicate image data
+        data = list(self.data)
+        image_columns = map(lambda i: data[i::self.w], range(self.w))
 
-#def block_entropy(block, columns):
-#    be = sum(entropy(i, i+1, columns) for i in range(block['start'], block['end']))
-#    return be / block['end']-block['start']
+        #returns the correct ordering of the blocks
+        ordered_block = self.merge()
 
-#def image_entropy(blocks, columns):
-#    return sum(map(lambda x, y: entropy(x[1], y[0], columns), blocks[:-1], blocks[1:]))
+        #arrange the columns according to the correct order
+        unshredded_columns = [image_columns[col] for block in ordered_block['parts'] 
+                                                 for col in range(block[0], block[1]+1)]
 
-
+        #create image data from columns
+        newdata = tuple(tuple(unshredded_columns[i][j][:3]) for j in range(self.h) 
+                                                            for i in range(self.w))
         
- 
+        img = Image.new('RGB', (self.w, self.h))
+        img.putdata(newdata)
+        return img
 
-
-#def sort(blocks, bw, w):
-#    columns = [bw[i::w] for i in range(0, w)]
-#    print "starting sort"
-
-#    for i in range(len(blocks)):
-#        first = blocks[i]
-#        #first_next = blocks[i]
-
-#       for j in range(len(blocks)-1):
-#           second = blocks[j]
-#           #second_next = blocks[j+1]
-
-            #status_entropy = (entropy(first[1], first_next[0], columns) +
-            #                  entropy(second[1], second_next[0], columns))
-            #change_entropy = (entropy(first[1], second_next[0], columns) +
-            #                  entropy(second[1], first_next[0], columns))
-
-#            new_blocks = list(blocks)
-#            new_blocks[i] = blocks[j]
-#            new_blocks[j] = blocks[i]
-#             
-#            ne = image_entropy(new_blocks, columns)
-#            oe = image_entropy(blocks, columns)
-
-            #print "Entropy:", ne, "<", oe
-
-#            if ne < oe:
-#                print "Switching", first, "with", second
-#                blocks = new_blocks
-#                #print "Old entropy", (origin[1], test[0]), old_entropy
-#                #print "New entropy", (new[1], test[0]), new_entropy
-#                #blocks[i] = second
-#                #blocks[j] = first
-#                return sort(blocks, bw, w)
-#    print blocks
-
-
-
-
-def unshred(img, dividers):
-    """Unshred an image according to the dividers.
-
-    Takes and image in PIL format and unshreds it according to the boundaries 
-    defined in dividers.
+def unshred(image, even=False, shred=None, bounds=None):
+    """Unshred an image according to the bounds.
 
     Arguments:
-    img --- image to be unshredded in PIL format.
-    dividers --- List containing the columns immediately before a division.
+    image --- file name of the image to be unshredded.
+    even --- Set to True if the boundaries are evenly distributed and size should be guessed.
+    shred --- Size of the evenly distributed shreds, if provided it may improve speed.
+    bounds --- List containing the columns index at the beginning of each shred.
+    output --- The file name where to save the file.
 
     Returns the unshredded image in PIL format.
     """
 
-    #values = []
-    #for i in range(0, img.size[0]-1):
-    #    values.append(compare_columns(img, i, i+1))
-
-    #print img.size
-    #print "image has", len(values), "columns"
-    #for i in range(1,18):
-    #    for j in range(-3, 3):
-    #        print values[32*i+j],
-    #    print
-    #make_difference_image(img)
-
-
-    #print "entropy 637-> 638", entropy(637, 638, columns)
-    #print "entropy 638-> 639", entropy(638, 639, columns)
-    #print "entropy 639-> 544", entropy(639, 544, columns)
-    #print "entropy 544-> 545", entropy(544, 545, columns)
-    #print "entropy 545-> 546", entropy(545, 546, columns)
-
+    img = Image.open(image)
     shredded_image = ShreddedImage(img)
-    shredded_image.gess_blocks()
+    if shred: bounds = range(0, img.size[0], shred)
 
-    #blocks = gess_dividers(bw, w, h)
-    dividers = range(0, shredded_image.w, 32)
-    blocks = map(lambda x, y: {'start': x, 'end': y-1, 'parts':[(x, y-1)]}, dividers, dividers[1:]+[640])
-    #for b in blocks:
-    #    b['entropy'] = block_entropy(b, columns)
-    #pprint(blocks)
-    #shredded_image.blocks = blocks
-
-    r = shredded_image.merge()
-
-    data = list(shredded_image.data)
-    image_columns = map(lambda i: data[i::shredded_image.w], range(shredded_image.w))
-
-    unshredded_columns = []
-    for p in r['parts']:
-        init = p[0]
-        end = p[1]
-        unshredded_columns += image_columns[init:end+1]
-
-    newdata = []
+    unshredded_img = shredded_image.unshred(even, bounds)
     
-    print len(unshredded_columns)
-    print len(unshredded_columns[0])
-
-    for j in range(shredded_image.h):
-        for i in range(shredded_image.w):
-            newdata.append(tuple(unshredded_columns[i][j][:3]))
-
-    result = Image.new('RGB', img.size)
-    result.putdata(newdata)
-    result.save('test.png')
-
+    return unshredded_img
 
 
 def main():
-    image = Image.open(sys.argv[1])
-    unshred(image, [])
-    #data = image.getdata()
+    parser = argparse.ArgumentParser(description='Unshred an image. You may provide the shredding size, otherwise it will be guessed from the image.',
+                                     epilog='You may specify an image with uneven shreds and they will be calculated, however this can be slow for large images.')
+    parser.add_argument('image', type=str, help='image file to unshred')
+    parser.add_argument('-e', '--even', action='store_true', help='specify that shredding is evenly distributed')
+    parser.add_argument('-s', '--shred', type=int, help='specify the size of the evenly distributed shreds')
+    parser.add_argument('-b', '--bounds', type=int, nargs='+', help='list of the boundaries of the shreds')
+    parser.add_argument('-o', '--output', type=str, help='output file')
+    args = parser.parse_args()
 
+    shredding_args = dict(vars(args))
+    del shredding_args['output']
 
+    img = unshred(**shredding_args)
+    
+    output=args.output
+
+    if not output:
+        dot = image.rindex('.')
+        output = image[:dot] + '.unshredded' + image[dot:]
+
+    img.save(output)
 
 if __name__ == '__main__':
     main()
